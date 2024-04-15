723fffdfcd9faecb73ef5c923c553d9b39e0b6b2
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-16 00:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function fibonacci(num) {
 // your code here
-	return fibonacci(num-1);
+	var fibSeries = [0, 1]; // Initialize the series with the first two terms
+    for (var i = 2; i < num; i++) {
+        var nextTerm = fibSeries[i - 1] + fibSeries[i - 2]; // Calculate the next term
+        fibSeries.push(nextTerm); // Add the next term to the series
+    }
+    return fibSeries;
 }
 
 module.exports = fibonacci;
